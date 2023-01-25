4619009470830e072dfa2f756c5c2abfdee6e577
ENIOLA SODIQ OLADIPUPO <49729441+Enisco@users.noreply.github.com>
2023-01-25 11:46:09 +0100

    on

--- lib/waste_bin_app.dart
+++ lib/waste_bin_app.dart
@@ -10,7 +10,7 @@ class WasteBinApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Smart Bin',
+      title: 'Smart Bin App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
